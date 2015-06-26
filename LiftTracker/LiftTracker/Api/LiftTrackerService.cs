8b6d014deb2b7226793fc61e257d7b2d565e8de2
Brian Wheat <brian.wheat84@gmail.com>
2015-06-26 15:46:55 -0400

    init

---
+++ LiftTracker/LiftTracker/Api/LiftTrackerService.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack;
+
+namespace LiftTracker.Api
+{
+    public class LiftTrackerService : Service
+    {
+        public object Any(LiftTrackerRequest request)
+        {
+            return new LiftTrackerResponse { Result = "Hello, " + request.Name };
+        }
+    }
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class LiftTrackerRequest
+    {
+        public string Name { get; set; }
+    }
+
+    public class LiftTrackerResponse
+    {
+        public string Result { get; set; }
+    }
+}
---
+++ LiftTracker/LiftTracker/Api/UserService.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack;
+namespace LiftTracker.Api
+{
+    public class UserService : Service
+    {
+        public object Post(AddUser request)
+        {
+            //add the user
+            return new AddUserResponse {UserId = 255};
+        }
+    }
+
+    public class AddUserResponse
+    {
+        public long UserId { get; set; }
+    }
+
+    [Route("/users", "POST")]
+    public class AddUser
+    {
+        public string Name { get; set; }
+        public int Goal { get; set; }
+    }
+}
